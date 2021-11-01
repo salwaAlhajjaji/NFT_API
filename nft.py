d6a29d26c0d9571f59820d5c39f06547895d023e
salwaAlhajjaji <salwa.alhajjaji@hotmail.com>
2021-11-01 14:00:13 +0300

    Update nft.py

--- nft.py
+++ nft.py
@@ -1,15 +1,20 @@
-import web3
+# import web3
 import streamlit as st
 import requests, json
 from web3 import Web3
 import pandas as pd
+# Most expensive NFT
+# Slider for specify the range of price 
+# Add the bundles as Endpoint => done
+# ##on sale as filter
+# ##owner as filter
 
 st.sidebar.header("Endpoints")
-endpoint_choices = ['Assets', 'Events', 'Rarity',  'Bundles']
+endpoint_choices = ['Most Expnsive NFT', 'Assets', 'Events', 'Rarity', 'Bundles']
 endpoint = st.sidebar.selectbox("Choose an Endpoint", endpoint_choices)
 
 st.title(f"OpenSea API Explorer - {endpoint}")
-
+# function for rendering assests info.
 def render_asset(asset):
     if asset['name'] is not None:
         st.subheader(asset['name'])
@@ -29,12 +34,22 @@ def render_asset(asset):
     elif asset['image_url']:
         st.image(asset['image_url'])
 
+# function for rendering assests from image_url for different types of media.
+def render_img(asset):
+    if asset.endswith('mp4') or asset.endswith('mov'):
+        st.video(asset)
+    elif asset.endswith('svg'):
+        svg = requests.get(asset).content.decode()
+        st.image(svg)
+    elif asset:
+        st.image(asset)
+
 
 if endpoint == 'Events':
     collection = st.sidebar.text_input("Collection")
     asset_contract_address = st.sidebar.text_input("Contract Address")
     token_id = st.sidebar.text_input("Token ID")
-    event_type = st.sidebar.selectbox("Event Type", ['offer_entered', 'cancelled', 'bid_withdrawn', 'transfer', 'approve'])
+    event_type = st.sidebar.selectbox("Event Type", ['','offer_entered', 'cancelled', 'bid_withdrawn', 'transfer', 'approve'])
     params = {}
     if collection:
         params['collection_slug'] = collection
@@ -50,7 +65,6 @@ if endpoint == 'Events':
     events = r.json()
     event_list = []
     for event in events['asset_events']:
-        # print(events['asset_events'])
         if event_type == 'offer_entered':
             if event['bid_amount']:
                 bid_amount = Web3.fromWei(int(event['bid_amount']), unit='ether')
@@ -114,22 +128,63 @@ if endpoint == 'Rarity':
             for trait in asset['traits']:
                 st.write(f"{trait['trait_type']} - {trait['value']} - {trait['trait_count']} have this")
 
-
 if endpoint == 'Bundles':
-    # params = {'limit': 2}
-    #params = {'on_sale': 'false'}
-    # params['num_sales'] =0;
-    r = requests.get('https://api.opensea.io/api/v1/bundles', )
-    
+    st.sidebar.header('Filters')
+    owner = st.sidebar.text_input("Owner")
+    numBun = st.sidebar.text_input("Number of Bundles")
+    price = st.sidebar.slider(label ='Current Price - (ETH)', min_value= 0.0, max_value= 100.0, step=0.01)
+    params = {}
+    if owner:
+        params['owner'] = owner
+    if numBun:
+        params['limit'] = int(numBun)
+
+    r = requests.get('https://api.opensea.io/api/v1/bundles', params=params)
+    price_list=[]
+    nowPrice=0.0
     events = r.json()['bundles']
-    event_list = []
     number =0
     for event in events:
         number= number+1
-        st.subheader(f'*** BUNDLE {number} *** {event["name"]}')
-        for asset in event['assets']:
-            # render_asset(asset)
-            st.subheader(asset['name'])
-            st.image(asset['image_url'])
-    st.write(events)
-    
+        for currP in event['sell_orders']:
+            currPeth = Web3.fromWei(float(currP['current_price']), unit='ether')
+            price_list.append({currP['current_price'] ,currPeth})
+            
+        if price > currPeth:
+            # bundle info
+            st.header(f'*** BUNDLE {number} *** | {event["name"]}')
+            st.write(event["description"])
+            st.write(f'** Current Price {str(currPeth)} ETH')
+            # assets info
+            for asset in event['assets']:
+                st.subheader(asset['name'])
+                st.write("number of sales "+str(asset['num_sales']))
+                render_img(asset['image_url'])
+            st.write("**************************************")
+
+if endpoint == 'Most Expnsive NFT':
+    r= requests.get('https://api.opensea.io/wyvern/v1/orders')
+    events = r.json()['orders']
+    expen=0.0
+    nft_obj = {}
+
+    # process to get most expensive NFT
+    for event in events:
+        price = Web3.fromWei(float(event['current_price']), unit='ether')
+        if expen < price:
+            expen =price
+            nft_obj = event
+
+    # display most expensive NFT info 
+    st.subheader(nft_obj['asset']['name'])
+    st.write(nft_obj['asset']['description'])
+    st.write(render_img(nft_obj['asset']['image_url']))
+    st.write("Current Price "+str(expen))
+    st.write("Owner Address: "+nft_obj['asset']['owner']['address'])
+    # st.write(nft_obj['asset'])
+
+        
+
+
+
+
