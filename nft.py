0d1d06c675b025162ed16bdb88f7ffd89c4b18b5
salwaAlhajjaji <salwa.alhajjaji@hotmail.com>
2021-11-01 14:09:50 +0300

    Update nft.py

--- nft.py
+++ nft.py
@@ -10,7 +10,7 @@ import pandas as pd
 # ##owner as filter
 
 st.sidebar.header("Endpoints")
-endpoint_choices = ['Most Expnsive NFT', 'Assets', 'Events', 'Rarity', 'Bundles']
+endpoint_choices = ['Most Expnsive NFT','Price', 'Assets', 'Events', 'Rarity', 'Bundles']
 endpoint = st.sidebar.selectbox("Choose an Endpoint", endpoint_choices)
 
 st.title(f"OpenSea API Explorer - {endpoint}")
@@ -184,6 +184,48 @@ if endpoint == 'Most Expnsive NFT':
     # st.write(nft_obj['asset'])
 
         
+if endpoint == 'Price':
+    
+    st.header('Pulling price data from *Binance API*')
+
+    st.subheader('**Selected Price**')
+
+    df = pd.read_json('https://api.binance.com/api/v3/ticker/24hr')
+
+    def round_value(input_value):
+        if input_value.values > 1:
+            a = float(round(input_value, 2))
+        else:
+            a = float(round(input_value, 8))
+        return a
+
+    crpytoList = {
+    
+        'Price ': 'ETHBUSD'
+    }
+
+    col1, col2, col3 = st.columns(3)
+
+    for i in range(len(crpytoList.keys())):
+        selected_crypto_label = list(crpytoList.keys())[i]
+        selected_crypto_index = list(df.symbol).index(crpytoList[selected_crypto_label])
+        selected_crypto = st.sidebar.selectbox(selected_crypto_label, df.symbol, selected_crypto_index, key = str(i))
+        col_df = df[df.symbol == selected_crypto]
+        col_price = round_value(col_df.weightedAvgPrice)
+        col_percent = f'{float(col_df.priceChangePercent)}%'
+    if i < 3:
+            with col1:
+                st.metric(selected_crypto, col_price, col_percent)
+    if 2 < i < 6:
+            with col2:
+                st.metric(selected_crypto, col_price, col_percent)
+    if i > 5:
+            with col3:
+                st.metric(selected_crypto, col_price, col_percent)
+
+    st.header('**All Price**')
+    st.dataframe(df)
+
 
 
 
